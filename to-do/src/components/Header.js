a8f3f1146355fa7e869ae180ee8310bf661e1ceb
Dmitriy Belozerov <dm.belozerov@yandex.ru>
2023-09-23 20:57:47 +0300

    install scss

---
+++ to-do/src/components/Header.js
@@ -0,0 +1,12 @@
+
+function Header() {
+    return (
+        <header className="header">
+            <h1 className="header__title">
+                Список дел
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
