15db0f04fc939344116fa39a4f2bb94c9c831be2
Dmitriy Belozerov <dm.belozerov@yandex.ru>
2023-11-22 00:26:50 +0300

    feat: api

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Main from './Main';
 
+import api from '../utils/api';
+
 function App() {
   const [arrTodo, setArrTodo] = React.useState([]);
+  // const [itemsTodo, setItemsTodo] = React.useState([]);
+
+  React.useEffect(() => {
+    api.getItemsTodo()
+      .then((data) => {
+        console.log(data)
+      })
+      .catch(err => console.log(err))
+  }, [])
 
   function handleAddTodo(point) {
     setArrTodo([point, ...arrTodo])
@@ -16,7 +27,7 @@ function App() {
 
   function handleMoveElement(index) {
     const newList = [...arrTodo];
-    newList.splice(index -1, 2, arrTodo[index], arrTodo[index - 1]);
+    newList.splice(index - 1, 2, arrTodo[index], arrTodo[index - 1]);
     setArrTodo(newList)
   }
 
---
+++ src/utils/api.js
@@ -0,0 +1,58 @@
+const apiOptions = {
+    baseUrl: "http://localhost:3000/itemtodo",
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    },
+
+}
+
+class Api {
+    constructor(config) {
+        this._header = config.headers;
+        this._baseUrl = config.baseUrl;
+    }
+
+    _getJsonOrError(res) {
+        if (res.ok) {
+            return res.json();
+        }
+        throw new Error(`Ошибочка при загрузке данных с сервера`)
+    }
+
+    getItemsTodo() {
+        return fetch(`${this._baseUrl}`, {
+            mode: 'no-cors',
+            credentials: 'include',
+            headers: this._header,
+        })
+            .then(this._getJsonOrError)
+    }
+
+    createItemTodo(newName, newLink) {
+        return fetch(`${this._baseUrl}`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: this._header,
+            body: JSON.stringify({
+                name: `${newName}`,
+                link: `${newLink}`
+            }),
+        })
+            .then(this._getJsonOrError)
+    }
+
+    deleteItemTodo(id) {
+        return fetch(`${this._baseUrl}/${id}`, {
+            method: 'DELETE',
+            credentials: 'include',
+            headers: this._header,
+        })
+            .then(this._getJsonOrError)
+    }
+}
+
+
+const api = new Api(apiOptions);
+
+export default api;
